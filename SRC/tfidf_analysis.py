56e9cf63a7fc15cdbf79d3fba0f68525c0e0f7b7
lucywang-uva <75697123+lucywang-uva@users.noreply.github.com>
2023-02-24 00:20:23 -0500

    added self written reviews

--- SRC/tfidf_analysis.py
+++ SRC/tfidf_analysis.py
@@ -38,7 +38,27 @@ knn = KNeighborsClassifier(n_neighbors=7)
 clf = knn.fit(X_train_tfidf, train.rating)
 
 # Input Data to predict their classes of the given categories
-docs_new = ['This is a great product', 'This is an ok product']
+docs_new = ['This is a great product. I would recommend this to my friends.', 
+            'I would use this tablet at work everyday.', 
+            "This product doesn't work as well as I expected.",
+            'I like this purchase and I would go back to buy more.', 
+            'I recently bought a new tablet and I have to say, I absolutely love it! It is great for work and is so easy to use.',
+            'I seldom use this tablet since is it so slow.',
+            'This product was ok', 
+            'This was an okay product',
+            'Do not buy this.',
+            'return',
+            'This product is great. I would buy this product again', 
+            'I hate this tablet and Amazon refused to offer me a refund.',
+            'I love how easy to use this tablet is', 
+            'Amazon charged me an extra fee. It took me a week to get my money back.',
+            'I am very satisfied with my purchase and would definitely consider buying more of these tablets again.', 
+            'I am really impressed with the quality of this product for the price.',
+            'Overall, I would recommend this tablet to others.', 
+            'I thought that this purchase would be a really good investment, but it broke the second day of using it.', 
+            "I returned this product as it came in damaged. I won't be purchasing tablets from Amazon ever again."
+            ]
+
 # building up feature vector of our input
 X_new_counts = count_vect.transform(docs_new)
 # We call transform instead of fit_transform because it's already been fit
